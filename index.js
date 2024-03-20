a97080ddd718ff54661742d2a94c02237c3236dd
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-03-20 20:49:33 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+    const hqLocation = 42;
+    if (42 < pickup) {
+        return pickup - 42; 
+    }else {
+        return 42 - pickup;
+    }
+}
+
+function distanceFromHqInFeet(pickupp){
+    distanceFromHqInBlocks(pickupp);
+    return distanceFromHqInBlocks(pickupp) * 264;
+}
+
+function distanceTravelledInFeet(start, destination){
+    const distance = Math.abs(destination - start) * 264;
+    return distance;
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400) {
+        return 0;
+    }else if (distance >400 && distance < 2000) {
+        const price = (distance - 400) * 0.02;
+        return price;
+    }else if (distance > 2000 && distance < 2500){
+        return 25;
+    }else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
